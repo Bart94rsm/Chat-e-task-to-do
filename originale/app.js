89f6e0e5db8e889922fe709dc3862f9c661991a7
Barto94rsm <barto94rsm@gmail.com>
2023-11-16 11:12:03 +0100

    first

---
+++ originale/app.js
@@ -0,0 +1,3 @@
+import init from './init.js';
+import chat from './init.js';
+import task from './init.js';
---
+++ originale/init.js
@@ -0,0 +1,11 @@
+const firstTab=document.querySelector('#chat');
+const appServicesWrapper=document.querySelector('#app-services');
+const serviceIconMap=new Map([
+    ['chat', document.querySelector('#chat-icon svg')], //chiave elemento array e come valore elemento svg
+    ['task', document.querySelector('#to-do-app svg')]
+]) 
+//seleziono gli svg, usando map con all'interno un array bidimensionale, ogni elemento sarà un elemento della mappa
+
+export default function(){
+    
+}
